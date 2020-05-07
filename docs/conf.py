7f56a390f557d05dcf7b2a3940db1d725de00353
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-05-07 17:18:42 +0200

    Prune config (no longer needed)

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,3 @@
 from crate.theme.rtd.conf.npgsql import *
 
 exclude_patterns = ["out/**"]
-
-# crate.theme sets html_favicon to favicon.png which causes a warning because
-# it should be a .ico and in addition there is no favicon.png in this project
-# so it can't find the file
-html_favicon = None
-
-site_url = 'https://crate.io/docs/npgsql/en/latest/'
-extensions = ['sphinx_sitemap']
